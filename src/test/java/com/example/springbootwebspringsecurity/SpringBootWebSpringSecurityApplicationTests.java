cdb12f4e4570d71622a88e80ee8e4e2b76428208
Sahak-S <sahak.sa@mail.ru>
2022-07-30 00:14:37 +0400

    spring - boot - spring - security

---
+++ src/test/java/com/example/springbootwebspringsecurity/SpringBootWebSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
